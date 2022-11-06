5cef30e1f32f01a711f3b5f93461376e9ef801a2
ap-stein <a_stein2001@yahoo.com>
2022-11-06 12:26:50 -0600

    Add project files.

---
+++ FullStack.API/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace FullStack.API.Controllers.Models
+{
+    public class Employee
+    { 
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public long Phone { get; set; }
+        public long Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
